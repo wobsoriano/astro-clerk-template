b48058e3ae1396ca8d3866622b45bf7665851a36
wobsoriano <sorianorobertc@gmail.com>
2024-07-17 12:07:50 -0700

    clean up prism

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,21 @@ export default defineConfig({
     clerk({
       signInForceRedirectUrl: "/dashboard",
       signUpForceRedirectUrl: "/dashboard",
+      appearance: {
+        variables: { colorPrimary: "#000000" },
+        elements: {
+          formButtonPrimary:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          socialButtonsBlockButton:
+            "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+          socialButtonsBlockButtonText: "font-semibold",
+          formButtonReset:
+            "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+          membersPageInviteButton:
+            "bg-black border border-black border-solid hover:bg-white hover:text-black",
+          card: "bg-[#fafafa]",
+        },
+      },
     }),
     react(),
     vue(),
