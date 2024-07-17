9a9c750f1cd907ddba1a4aa8bb4538f7e3734764
wobsoriano <sorianorobertc@gmail.com>
2024-07-16 19:09:45 -0700

    add layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte(), tailwind()]
+  integrations: [react(), vue(), svelte(), tailwind()],
 });
