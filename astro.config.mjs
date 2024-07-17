cb712e22a487a2c3d4d0f9acb84635d5a178c128
wobsoriano <sorianorobertc@gmail.com>
2024-07-16 18:57:57 -0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte()]
+  integrations: [react(), vue(), svelte(), tailwind()]
 });
