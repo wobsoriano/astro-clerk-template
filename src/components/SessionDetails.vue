878d5b2b8b2236671aaed20002a3e0c425680e3b
wobsoriano <sorianorobertc@gmail.com>
2024-07-16 23:16:46 -0700

    make examples work

--- src/components/SessionDetails.vue
+++ src/components/SessionDetails.vue
@@ -1,54 +1,52 @@
 <script setup lang="ts">
 import { $sessionStore } from "@clerk/astro/client";
 import { useStore } from "@nanostores/vue";
-import { watchEffect } from "vue";
-const session = useStore($sessionStore);
 
-watchEffect(() => {
-  console.log("session", session.value);
-});
+const session = useStore($sessionStore);
 </script>
 
 <template>
-  <div v-if="session">
-    <div class="pb-6 max-h-96">
-      <dl>
-        <div class="px-8 py-2">
-          <dt class="text-sm font-semibold">Session ID</dt>
-          <dd
-            class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2"
+  <div v-if="session" class="pb-6 max-h-96">
+    <dl>
+      <div class="px-8 py-2">
+        <dt class="text-sm font-semibold">Session ID</dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
+          {{ session.id }}
+          <!-- <CopyButton :text="session.id" /> -->
+        </dd>
+      </div>
+      <div class="px-8 py-2">
+        <dt class="text-sm font-semibold mb-1">Status</dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+          <span
+            v-if="session.status === 'active'"
+            class="text-xs bg-success-50 text-success-700 flex w-min gap-1 px-2 py-[1px] rounded-2xl font-medium"
           >
-            {{ session.id }}
-            <!-- <CopyButton :text="session.id" /> -->
-          </dd>
-        </div>
-        <div class="px-8 py-2">
-          <dt class="text-sm font-semibold mb-1">Status</dt>
-          <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-            <span
-              v-if="session.status === 'active'"
-              class="text-xs bg-success-50 text-success-700 flex w-min gap-1 px-2 py-[1px] rounded-2xl font-medium"
-            >
-              <div class="m-auto">
-                <!-- <Icon name="Dot" /> -->
-              </div>
-              Active
-            </span>
-          </dd>
-        </div>
-        <div class="px-8 py-2">
-          <dt class="text-sm font-semibold mb-1">Last Active</dt>
-          <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-            {{ session.lastActiveAt.toLocaleString() }}
-          </dd>
-        </div>
-        <div class="px-8 py-2">
-          <dt class="text-sm font-semibold mb-1">Expiry</dt>
-          <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-            {{ session.expireAt.toLocaleString() }}
-          </dd>
-        </div>
-      </dl>
-    </div>
+            <div class="m-auto">
+              <!-- <Icon name="Dot" /> -->
+            </div>
+            Active
+          </span>
+        </dd>
+      </div>
+      <div class="px-8 py-2">
+        <dt class="text-sm font-semibold mb-1">Last Active</dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+          {{ session.lastActiveAt.toLocaleString() }}
+        </dd>
+      </div>
+      <div class="px-8 py-2">
+        <dt class="text-sm font-semibold mb-1">Expiry</dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+          {{ session.expireAt.toLocaleString() }}
+        </dd>
+      </div>
+    </dl>
+  </div>
+  <div
+    v-else
+    class="text-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+  >
+    Loading user data...
   </div>
 </template>
--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,45 +1,67 @@
-import { $sessionStore } from "@clerk/astro/client";
+import { $userStore } from "@clerk/astro/client";
 import { useStore } from "@nanostores/react";
 
 export default function SessionDetails() {
-  const session = useStore($sessionStore);
+  const user = useStore($userStore);
 
   return (
-    <div>
-      <div className="pb-6 max-h-96">
-        <dl>
-          <div className="px-8 py-2">
-            <dt className="text-sm font-semibold">Session ID</dt>
-            <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
-              {session?.id}
-              {/* <CopyButton text={session.id} /> */}
-            </dd>
-          </div>
-          <div className="px-8 py-2">
-            <dt className="text-sm font-semibold mb-1">Status</dt>
-            <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-              {session?.status === `active` && (
-                <span className="text-xs bg-success-50 text-success-700 flex w-min gap-1 px-2 py-[1px] rounded-2xl font-medium">
-                  <div className="m-auto">{/* <Dot /> */}</div>
-                  Active
-                </span>
-              )}
-            </dd>
-          </div>
-          <div className="px-8 py-2">
-            <dt className="text-sm font-semibold mb-1">Last Active</dt>
-            <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-              {session?.lastActiveAt.toLocaleString()}
-            </dd>
-          </div>
-          <div className="px-8 py-2">
-            <dt className="text-sm font-semibold mb-1">Expiry</dt>
-            <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-              {session?.expireAt.toLocaleString()}
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </div>
+    <>
+      {user ? (
+        <div className="pb-6 max-h-96">
+          <dl>
+            <div className="px-8 py-2">
+              <dt className="text-sm font-semibold">User ID</dt>
+              <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
+                {user.id}
+                {/* <CopyButton text={user.id} /> */}
+              </dd>
+            </div>
+            {user.firstName && (
+              <div className="px-8 py-2">
+                <dt className="text-sm font-semibold mb-1">First Name</dt>
+                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+                  {user.firstName}
+                </dd>
+              </div>
+            )}
+            {user.lastName && (
+              <div className="px-8 py-2">
+                <dt className="text-sm font-semibold mb-1">Last Name</dt>
+                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+                  {user.lastName}
+                </dd>
+              </div>
+            )}
+            <div className="px-8 py-2">
+              <dt className="text-sm font-semibold mb-1">Email addresses</dt>
+              <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+                {user.emailAddresses.map((email) => (
+                  <div key={email.id} className="flex gap-2 mb-1">
+                    {email.emailAddress}
+                    {user.primaryEmailAddressId === email.id && (
+                      <span className="text-xs bg-primary-50 text-primary-700 rounded-2xl px-2 font-medium pt-[2px]">
+                        Primary
+                      </span>
+                    )}
+                  </div>
+                ))}
+              </dd>
+            </div>
+            {user.imageUrl && (
+              <div className="px-8 py-2">
+                <dt className="text-sm font-semibold mb-1">Profile Image</dt>
+                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+                  <img src={user.imageUrl} className="rounded-full w-12 h-12" />
+                </dd>
+              </div>
+            )}
+          </dl>
+        </div>
+      ) : (
+        <div className="text-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          Loading user data...
+        </div>
+      )}
+    </>
   );
 }
