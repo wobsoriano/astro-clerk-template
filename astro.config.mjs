d29c2ab114833915433476ddcf3995fea86b8543
wobsoriano <sorianorobertc@gmail.com>
2024-07-17 00:09:18 -0700

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelServerless from "@astrojs/vercel/serverless";
 import clerk from "@clerk/astro";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk({
-    signInForceRedirectUrl: '/dashboard',
-    signUpForceRedirectUrl: '/dashboard',
-  }), react(), vue(), svelte(), tailwind()],
+  integrations: [
+    clerk({
+      signInForceRedirectUrl: "/dashboard",
+      signUpForceRedirectUrl: "/dashboard",
+    }),
+    react(),
+    vue(),
+    svelte(),
+    tailwind(),
+  ],
   output: "server",
   adapter: vercelServerless(),
 });
