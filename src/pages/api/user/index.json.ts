bfae43f2526d02d887518cbcbd84d84e7db5ecd8
wobsoriano <sorianorobertc@gmail.com>
2024-08-28 08:40:32 -0700

    Add test api user

---
+++ src/pages/api/user/index.json.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from "astro"
+
+export const GET: APIRoute = async ({ locals }) => {
+  const user = await locals.currentUser()
+  return new Response(
+    JSON.stringify({
+      user
+    })
+  )
+}
