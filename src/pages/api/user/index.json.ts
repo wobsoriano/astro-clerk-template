d23d49eda32349550e828540a5268595eed72fe5
wobsoriano <sorianorobertc@gmail.com>
2024-08-28 08:48:26 -0700

    simplify test api route

--- src/pages/api/user/index.json.ts
+++ src/pages/api/user/index.json.ts
@@ -1,10 +1,9 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ locals }) => {
-  const user = await locals.currentUser();
   return new Response(
     JSON.stringify({
-      user,
+      hello: 'world',
     }),
   );
 };
