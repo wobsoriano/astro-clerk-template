ab18affdab71dfd907659c90bee0fe0f5597fc86
wobsoriano <sorianorobertc@gmail.com>
2024-08-30 11:14:51 -0700

    remove test route

--- src/pages/api/user/index.json.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async ({ locals }) => {
-  return new Response(
-    JSON.stringify({
-      hello: 'world',
-    }),
-  );
-};
