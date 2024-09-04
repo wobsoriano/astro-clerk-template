3484a70ac259ab1b235f387f0c9f78035ee2a7b4
wobsoriano <sorianorobertc@gmail.com>
2024-09-04 16:48:09 -0700

    revert previous commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
-import { sequence } from "astro/middleware";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export const onRequest = sequence(
-  async (_ctx, next) => {
-    const resp = await next();
-    resp.headers.set('Netlify-CDN-Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
-    return resp;
-  },
-  clerkMiddleware((auth, context) => {
-    const { userId, redirectToSignIn } = auth();
-    if (!userId && isProtectedRoute(context.request)) {
-      return redirectToSignIn();
-    }
-  })
-);
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId, redirectToSignIn } = auth();
+  if (!userId && isProtectedRoute(context.request)) {
+    return redirectToSignIn();
+  }
+});
