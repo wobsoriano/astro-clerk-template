146cbd1590e818b2a3ae5227e24c51d18ce9a724
wobsoriano <sorianorobertc@gmail.com>
2024-12-09 09:52:56 -0800

    test netlify fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+import { sequence } from "astro:middleware";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export const onRequest = clerkMiddleware((auth, context) => {
-  const { userId, redirectToSignIn } = auth();
-  if (!userId && isProtectedRoute(context.request)) {
-    return redirectToSignIn();
-  }
-});
+export const onRequest = sequence(
+  async function auth(a, next) {
+    const response = await next();
+    response.headers.set("Netlify-Vary", "cookie=__session");
+    return response
+  },
+  clerkMiddleware((auth, context) => {
+    const { userId, redirectToSignIn } = auth();
+    if (!userId && isProtectedRoute(context.request)) {
+      return redirectToSignIn();
+    }
+  }),
+);
