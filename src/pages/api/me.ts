423879adcd370feb85aa86f780b51f0dbc334c85
wobsoriano <sorianorobertc@gmail.com>
2024-12-10 12:17:40 -0800

    netlify fix

--- src/pages/api/me.ts
+++ src/pages/api/me.ts
@@ -2,7 +2,6 @@ import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ params, request }) => {
   return Response.json({
-    // @ts-expect-error: todo
     isNetlify: import.meta.env.NETLIFY
   })
 }
